1b8e9bd428afd2241087adfc1e052ee18ca0a487
Ilia K <iliakib24@gmail.com>
2019-12-03 19:05:20 +0300

    update model

--- 3D.js
+++ 3D.js
@@ -1,16 +1,18 @@
+
 var container, scene, camera, renderer, controls;
 var positions;
 //var lines = 1000;
 var buffer_geometry;
+	var buffer_geometry_crossbar;
 
 var fibersData = [];
 var ArrayDeltaY = [];
 
 var minX, maxX, lengthFiber, sizeY, sizeZ, timeSpeed;
 
-minX = -500;
-maxX = 500;
-lengthFiber =50;
+minX = -1000;
+maxX = 1000;
+lengthFiber =150;
 sizeY = 100;
 sizeZ = 100;
 timeSpeed = 10;
@@ -186,6 +188,9 @@ function animate() {
 	    createGeomData();
 	    buffer_geometry.attributes.position.needsUpdate = true;
 	    buffer_geometry.computeBoundingSphere();
+
+	    	//buffer_mesh_crossbar.rotateX ( 0.01 );
+
 	    renderer.render(scene, camera);
 	}
 }
@@ -195,14 +200,16 @@ function init (){
 	container = document.getElementById( 'container' );
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color( 0xffffff );
-	camera = new THREE.PerspectiveCamera( 75, container.offsetWidth/container.offsetHeight, 0.1, 10000 );
+	camera = new THREE.PerspectiveCamera( 45, container.offsetWidth/container.offsetHeight, 0.1, 10000 );
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( container.offsetWidth, container.offsetHeight );
 	container.appendChild( renderer.domElement );
 	controls = new THREE.OrbitControls( camera, renderer.domElement );
 
 	//camera.position.x = (minX+maxX)/2;
-	camera.position.z = 500;
+	camera.position.z = 1000;
+
+		rollers();
 
 	createModelArrayY( maxX - minX );
 
@@ -238,4 +245,196 @@ fibersData = [
 
 	[NaN]		// time
 ];
+}
+
+
+
+
+function rollers() {
+
+	var materialR = new THREE.LineBasicMaterial({
+	color: 0xff0000 });
+	var materialB = new THREE.LineBasicMaterial({
+	color: 0x0000ff });
+
+var geometry_1 = new THREE.Geometry();
+geometry_1.vertices.push(
+	new THREE.Vector3( -10, -sizeY-10, 0 ),
+	new THREE.Vector3( 0, -sizeY, 0 ),
+	new THREE.Vector3( 0, -sizeY, 0 ),
+	new THREE.Vector3( 10, -sizeY-10, 0 ),
+	new THREE.Vector3( -10, sizeY+10, 0 ),
+	new THREE.Vector3( 0, sizeY, 0 ),
+	new THREE.Vector3( 0, sizeY, 0 ),
+	new THREE.Vector3( 10, sizeY+10, 0 ),
+
+	new THREE.Vector3( minX*0.9-10, -sizeY-10, 0 ),
+	new THREE.Vector3( minX*0.9, -sizeY, 0 ),
+	new THREE.Vector3( minX*0.9, -sizeY, 0 ),
+	new THREE.Vector3( minX*0.9+10, -sizeY-10, 0 ),
+	new THREE.Vector3( minX*0.9-10, sizeY + 10, 0 ),
+	new THREE.Vector3( minX*0.9, sizeY, 0 ),
+	new THREE.Vector3( minX*0.9, sizeY, 0 ),
+	new THREE.Vector3( minX*0.9+10, sizeY+10, 0 ),
+
+	new THREE.Vector3( maxX*0.9-10, -sizeY-10, 0 ),
+	new THREE.Vector3( maxX*0.9, -sizeY, 0 ),
+	new THREE.Vector3( maxX*0.9, -sizeY, 0 ),
+	new THREE.Vector3( maxX*0.9+10, -sizeY-10, 0 ),
+	new THREE.Vector3( maxX*0.9-10, sizeY+10, 0 ),
+	new THREE.Vector3( maxX*0.9, sizeY, 0 ),
+	new THREE.Vector3( maxX*0.9, sizeY, 0 ),
+	new THREE.Vector3( maxX*0.9+10, sizeY+10, 0 )
+);
+
+var geometry_2 = new THREE.Geometry();
+geometry_2.vertices.push(
+
+	new THREE.Vector3( minX-10, 0, -sizeZ ),
+	new THREE.Vector3( maxX+10, 0, -sizeZ ),
+	new THREE.Vector3( minX-10, 0, sizeZ ),
+	new THREE.Vector3( maxX+10, 0, sizeZ )
+);
+
+var line_1 = new THREE.LineSegments( geometry_1, materialB );
+var line_2 = new THREE.LineSegments( geometry_2, materialR );
+
+scene.add( line_1, line_2 );
+
+
+
+
+/*
+var roll_material = new THREE.LineBasicMaterial({
+	color: 0x888888 });
+
+var roll_geometry = new THREE.Geometry();
+*/
+var rolls_segments = 64;
+var roll_rad = 300;
+var deltaRadRoll = 2*Math.PI/rolls_segments;
+
+var roll_fi = 0;
+/*
+var xr1 = roll_rad*Math.cos(roll_fi);
+var yr1 = roll_rad*Math.sin(roll_fi);
+var xr2 = 0;
+var yr2 = 0;
+
+for (var i = 0; i < rolls_segments; i++) {
+	roll_fi += deltaRadRoll;
+	
+	xr2 = roll_rad*Math.cos(roll_fi);
+	yr2 = roll_rad*Math.sin(roll_fi);
+
+	roll_geometry.vertices.push(
+	new THREE.Vector3( xr1, yr1, 0 ),
+	new THREE.Vector3( xr2, yr2, 0 )
+	);
+
+	xr1 = xr2;
+	yr1 = yr2;
+};
+
+var roll_1 = new THREE.LineSegments( roll_geometry, roll_material );
+
+roll_1.position.set(0,roll_rad,0);
+scene.add( roll_1 );
+*/
+
+//var positions_rolls;
+var npr = 18;
+var buffer_geometry_rolls;
+buffer_geometry_rolls = new THREE.BufferGeometry();
+buffer_geometry_rolls.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array(rolls_segments * npr), 3 ));
+var positions_rolls = buffer_geometry_rolls.attributes.position.array;
+//createGeomData();
+
+var xr1 = roll_rad*Math.cos(roll_fi);
+var yr1 = roll_rad*Math.sin(roll_fi);
+var xr2 = 0;
+var yr2 = 0;
+
+	for ( var i = 0; i < rolls_segments; i++ ) {
+		roll_fi += deltaRadRoll;
+
+		xr2 = roll_rad*Math.cos(roll_fi);
+		yr2 = roll_rad*Math.sin(roll_fi);
+
+    	positions_rolls[ i * npr + 0 ] = xr1;
+        positions_rolls[ i * npr + 1 ] = yr1;
+        positions_rolls[ i * npr + 2 ] = -sizeZ;
+        positions_rolls[ i * npr + 3 ] = xr2;
+        positions_rolls[ i * npr + 4 ] = yr2;
+        positions_rolls[ i * npr + 5 ] = -sizeZ;
+
+        positions_rolls[ i * npr + 0 + 6 ] = xr1;
+        positions_rolls[ i * npr + 1 + 6 ] = yr1;
+        positions_rolls[ i * npr + 2 + 6 ] = sizeZ;
+        positions_rolls[ i * npr + 3 + 6 ] = xr2;
+        positions_rolls[ i * npr + 4 + 6 ] = yr2;
+        positions_rolls[ i * npr + 5 + 6 ] = sizeZ;
+
+        positions_rolls[ i * npr + 0 + 6 + 6 ] = xr1;
+        positions_rolls[ i * npr + 1 + 6 + 6 ] = yr1;
+        positions_rolls[ i * npr + 2 + 6 + 6 ] = 0;
+        positions_rolls[ i * npr + 3 + 6 + 6 ] = xr2;
+        positions_rolls[ i * npr + 4 + 6 + 6 ] = yr2;
+        positions_rolls[ i * npr + 5 + 6 + 6 ] = 0;
+
+        xr1 = xr2;
+		yr1 = yr2;
+    };
+
+var material_rolls = new THREE.LineBasicMaterial({ color: 0x0, opacity: 0.1, transparent: true });
+var buffer_mesh_rolls = new THREE.LineSegments( buffer_geometry_rolls, material_rolls );
+buffer_mesh_rolls.position.set(0,roll_rad,0);
+scene.add( buffer_mesh_rolls );
+
+
+
+
+
+var crossbar_segments = 24;
+var deltaRadCrossbar = 2*Math.PI/crossbar_segments;
+var crossbar_fi = 0;
+
+
+buffer_geometry_crossbar = new THREE.BufferGeometry();
+buffer_geometry_crossbar.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array(crossbar_segments * 6), 3 ));
+var positions_crossbar = buffer_geometry_crossbar.attributes.position.array;
+//createGeomData();
+
+var xc1 = 0;
+var yc1 = 0;
+
+
+	for ( var i = 0; i < crossbar_segments; i++ ) {
+		crossbar_fi += deltaRadCrossbar;
+
+		xc1 = roll_rad*Math.cos(crossbar_fi);
+		yc1 = roll_rad*Math.sin(crossbar_fi);
+
+    	positions_crossbar[ i * 6 + 0 ] = xc1;
+        positions_crossbar[ i * 6 + 1 ] = yc1;
+        positions_crossbar[ i * 6 + 2 ] = -sizeZ;
+        positions_crossbar[ i * 6 + 3 ] = xc1;
+        positions_crossbar[ i * 6 + 4 ] = yc1;
+        positions_crossbar[ i * 6 + 5 ] = sizeZ;
+
+    };
+
+var material_crossbar = new THREE.LineBasicMaterial({ color: 0x0, opacity: 0.1, transparent: true });
+var buffer_mesh_crossbar = new THREE.LineSegments( buffer_geometry_crossbar, material_crossbar );
+buffer_mesh_crossbar.position.set(0,roll_rad,0);
+scene.add( buffer_mesh_crossbar );
+
+var buffer_mesh_rolls_d = buffer_mesh_rolls.clone();
+buffer_mesh_rolls_d.position.set(0,-roll_rad,0);
+scene.add( buffer_mesh_rolls_d );
+
+var buffer_mesh_crossbar_d = buffer_mesh_crossbar.clone();
+buffer_mesh_crossbar_d.position.set(0,-roll_rad,0);
+scene.add( buffer_mesh_crossbar_d );
+
 }
